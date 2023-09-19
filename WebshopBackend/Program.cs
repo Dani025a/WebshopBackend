6bbef48c33ac94d48877a8a56a85f4d1bdd452fd
Dani025a <dani025a@edu.easj.dk>
2023-09-19 14:07:44 +0200

    Delete controllers added new controllers

--- WebshopBackend/Program.cs
+++ WebshopBackend/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebshopBackend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WebshopContext>(
+options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("WebsiteDB"),
+    Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.23-mysql"));
+});
+
+
+
+
 
 var app = builder.Build();
 
